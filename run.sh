a3ca2960ac3246d7e689c8fd2dfc855d8f9a1a85
Samuel Ramos <alu0100815146@ull.edu.es>
2016-11-09 16:19:42 +0000

    Añadiendo passport con el ejemplo de libro

--- run.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/local/bin/bash
-# CLIENT_ID=__GITHUB_CLIENT_ID__ CLIENT_SECRET=__GITHUB_CLIENT_SECRET__ node server.js
--- server.js
+++ server.js
@@ -1,93 +1,70 @@
 var express = require('express');
 var passport = require('passport');
-//var Strategy = require('passport-facebook').Strategy;
 var Strategy = require('passport-github').Strategy;
 
-
-// Configure the GitHub strategy for use by Passport.
-//
-// OAuth 2.0-based strategies require a `verify` function which receives the
-// credential (`accessToken`) for accessing the GitHub API on the user's
-// behalf, along with the user's profile.  The function must invoke `cb`
-// with a user object, which will be set at `req.user` in route handlers after
-// authentication.
 passport.use(new Strategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/login/github/return'
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    // In this example, the user's GitHub profile is supplied as the user
-    // record.  In a production-quality application, the GitHub profile should
-    // be associated with a user record in the application's database, which
-    // allows for account linking and authentication with other identity
-    // providers.
-    return cb(null, profile);
-  }));
+        clientID: "676a763159b83edb09b9",
+        clientSecret: "9698a41ff1224a91c72edca35f48b9855ee8190d",
+        callbackURL: 'http://localhost:3000/login/github/return'
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        return cb(null, profile);
+    }));
 
-
-// Configure Passport authenticated session persistence.
-//
-// In order to restore authentication state across HTTP requests, Passport needs
-// to serialize users into and deserialize users out of the session.  In a
-// production-quality application, this would typically be as simple as
-// supplying the user ID when serializing, and querying the user record by ID
-// from the database when deserializing.  However, due to the fact that this
-// example does not have a database, the complete Twitter profile is serialized
-// and deserialized.
 passport.serializeUser(function(user, cb) {
-  cb(null, user);
+    cb(null, user);
 });
 
 passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
+    cb(null, obj);
 });
 
 
-// Create a new Express application.
 var app = express();
 
-// Configure view engine to render EJS templates.
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-// Use application-level middleware for common functionality, including
-// logging, parsing, and session handling.
+app.use(express.static(__dirname + '/gh-pages'));
 app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
-
-// Initialize Passport and restore authentication state, if any, from the
-// session.
+app.use(require('body-parser').urlencoded({
+    extended: true
+}));
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.get('/libro', function(req, res) {
+  res.sendFile(__dirname + '/gh-pages/readme.html');
+});
 
-// Define routes.
-app.get('/',
-  function(req, res) {
-    res.render('home', { user: req.user });
-  });
+app.get('/', function(req, res) {
+    res.render('home', {
+        user: req.user
+    });
+});
 
-app.get('/login',
-  function(req, res){
+app.get('/login', function(req, res) {
     res.render('login');
-  });
+});
 
-app.get('/login/github',
-  passport.authenticate('github'));
+app.get('/login/github', passport.authenticate('github'));
 
-app.get('/login/github/return', 
-  passport.authenticate('github', { failureRedirect: '/login' }),
-  function(req, res) {
+app.get('/login/github/return', passport.authenticate('github', {
+    failureRedirect: '/login'
+}), function(req, res) {
     res.redirect('/');
-  });
+});
 
-app.get('/profile',
-  require('connect-ensure-login').ensureLoggedIn(),
-  function(req, res){
-    res.render('profile', { user: req.user });
-  });
+app.get('/profile', require('connect-ensure-login').ensureLoggedIn(), function(req, res) {
+    res.render('profile', {
+        user: req.user
+    });
+});
 
 app.listen(3000);
